afd42f759e1347374a608f374e7c7053408bae55
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-04-12 18:07:00 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const form = document.querySelector("#create-task-form")
+  const input = document.querySelector("#new-task-description")
+  const submit = document.querySelector("#submit")
+  const tasks = document.querySelector("#tasks")
+
+  form.addEventListener("submit", handleSubmit)
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    function addTasks() {
+      const newTask = document.createElement("li")
+      newTask.innerHTML = `${input.value}`
+      tasks.appendChild(newTask)
+    }
+    addTasks()
+    input.value = ""
+  }
+
+
 });
